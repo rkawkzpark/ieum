dc009a0e5f06ab7787844e728b08723c84aff9f5
rkawkzpark <hungrygamja0106@gmail.com>
2025-06-26 23:20:08 +0900

    feat: Add User entity and BaseTimeEntity for auditing

---
+++ src/main/java/com/ieum/domain/User.java
@@ -0,0 +1,39 @@
+package com.ieum.domain;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+@Table(name = "users")
+public class User extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String studentId;
+
+    @Builder
+    public User(String email, String password, String name, String studentId) {
+        this.email = email;
+        this.password = password;
+        this.name = name;
+        this.studentId = studentId;
+    }
+}
