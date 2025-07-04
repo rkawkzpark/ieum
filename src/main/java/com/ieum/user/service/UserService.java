720eff81f742c42a8674592424670fd4887b56c6
rkawkzpark <hungrygamja0106@gmail.com>
2025-07-04 13:51:38 +0900

    feat: Implement user profile service logic

--- src/main/java/com/ieum/user/service/UserService.java
+++ src/main/java/com/ieum/user/service/UserService.java
@@ -3,7 +3,9 @@ package com.ieum.user.service;
 import com.ieum.common.exception.BusinessException;
 import com.ieum.common.exception.ErrorCode;
 import com.ieum.user.domain.User;
+import com.ieum.user.dto.request.ProfileUpdateRequest;
 import com.ieum.user.dto.request.SignUpRequest;
+import com.ieum.user.dto.response.ProfileResponse;
 import com.ieum.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -23,17 +25,30 @@ public class UserService {
 
         // 중복 email 검증
         if (userRepository.existsByEmail(request.getEmail())) {
-            // 수정된 ErrorCode를 사용합니다.
             throw new BusinessException(ErrorCode.DUPLICATE_RESOURCE);
         }
 
         // 중복 학번 검증
         if (userRepository.existsByStudentId(request.getStudentId())) {
-            // 수정된 ErrorCode를 사용합니다.
             throw new BusinessException(ErrorCode.DUPLICATE_RESOURCE);
         }
 
         User user = request.toEntity(passwordEncoder);
         userRepository.save(user);
+    }
+
+    public ProfileResponse getUserProfile(Long userId) {
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new BusinessException(ErrorCode.USER_NOT_FOUND));
+
+        return ProfileResponse.from(user);
+    }
+
+    @Transactional
+    public void updateUserProfile(Long userId, ProfileUpdateRequest request) {
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new BusinessException(ErrorCode.USER_NOT_FOUND));
+
+        user.updateProfile(request.getName(), request.getIntroduction());
     }
 }
