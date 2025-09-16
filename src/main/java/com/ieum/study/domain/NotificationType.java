0c20a8221a4424ffc18bddec194e3ae3b3081b72
rkawkzpark <hungrygamja0106@gmail.com>
2025-09-16 17:56:27 +0900

    feat(notification): Add Notification entity for user alerts

---
+++ src/main/java/com/ieum/study/domain/NotificationType.java
@@ -0,0 +1,7 @@
+package com.ieum.study.domain;
+
+public enum NotificationType {
+    NEW_APPLICATION,        // 새로운 스터디 지원
+    APPLICATION_APPROVED,   // 지원 승인
+    APPLICATION_REJECTED    // 지원 거절
+}
